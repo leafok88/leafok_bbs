7138ab35dc693c897d6ac4d6061785ed04964483
Leaflet <leaflet@leafok.com>
2025-04-17 12:49:51 +0800

    Refine and bug fix

--- manage/db_cleanup.php
+++ manage/db_cleanup.php
@@ -6,15 +6,14 @@
 
 	require_once "../lib/db_open.inc.php";
 	require_once "../bbs/session_init.inc.php";
-?>
-<?
-force_login();
 
-if (!$_SESSION["BBS_priv"]->checklevel(P_ADMIN_M | P_ADMIN_S) && !isset($_SERVER["argc"]))
-{
-	echo ("没有权限！");
-	exit();
-}
+	force_login();
+
+	if (!$_SESSION["BBS_priv"]->checklevel(P_ADMIN_M | P_ADMIN_S) && !isset($_SERVER["argc"]))
+	{
+		echo ("没有权限！");
+		exit();
+	}
 ?>
 <html>
 <head>
@@ -24,274 +23,306 @@ if (!$_SESSION["BBS_priv"]->checklevel(P_ADMIN_M | P_ADMIN_S) && !isset($_SERVER
 </head>
 <body>
 <?
-$sql = "DELETE FROM email WHERE complete AND set_dt < SUBDATE(NOW(), INTERVAL 180 DAY)";
-mysqli_query($db_conn, $sql)
-	or die("Delete mail error!");
-
-$sql = "UPDATE email SET complete = 0, error = 0
-		WHERE error = 1 AND set_dt > SUBDATE(NOW(), INTERVAL 7 DAY)";
-mysqli_query($db_conn, $sql)
-	or die("Update email status error!");
-
-$sql = "DELETE FROM visit_log WHERE dt < SUBDATE(NOW(), INTERVAL $BBS_normal_log_retention DAY)";
-mysqli_query($db_conn, $sql)
-	or die("Delete visit_log error!");
-
-$sql = "DELETE FROM user_online WHERE last_tm < SUBDATE(NOW(), INTERVAL $BBS_session_lifetime SECOND)";
-$rs = mysqli_query($db_conn, $sql);
-if ($rs == false)
-{
-	echo ("Delete online user error: " . mysqli_error($db_conn));
-	exit();
-}
-
-$sql = "DELETE FROM user_login_log WHERE login_dt < SUBDATE(NOW(), INTERVAL $BBS_critical_log_retention DAY)";
-mysqli_query($db_conn, $sql)
-	or die("Delete user_login_log error!");
-
-$sql = "DELETE FROM user_err_login_log WHERE login_dt < SUBDATE(NOW(), INTERVAL $BBS_normal_log_retention DAY)";
-mysqli_query($db_conn, $sql)
-	or die("Delete user_err_login_log error!");
-
-$sql = "DELETE FROM bbs_article_op WHERE op_dt < SUBDATE(NOW(), INTERVAL $BBS_critical_log_retention DAY)";
-mysqli_query($db_conn, $sql)
-	or die("Delete bbs_article_op error!");
-
-$sql = "DELETE FROM bbs_msg WHERE send_dt < SUBDATE(NOW(), INTERVAL $BBS_user_msg_retention DAY) AND (NOT new)";
-mysqli_query($db_conn, $sql)
-	or die("Delete bbs_msg error!");
-
-$sql = "DELETE FROM view_article_log WHERE dt < SUBDATE(NOW(), INTERVAL $BBS_new_article_period DAY)";
-mysqli_query($db_conn, $sql)
-	or die("Delete view_article_log error!");
-
-//Cleanup out-of-date non-excerptional topic
-$sql = "SELECT SID, topic_retention FROM section_config
-		WHERE enable AND topic_retention > 0";
-$rs = mysqli_query($db_conn, $sql);
-if ($rs == false)
-{
-	echo ("Query section info error: " . mysqli_error($db_conn));
-	exit();
-}
-
-while ($row = mysqli_fetch_array($rs))
-{
-	$sql = "SELECT AID FROM bbs WHERE TID = 0 AND SID = " . $row["SID"] .
-		" AND visible = 1 AND excerption = 0 AND sub_dt < SUBDATE(NOW(), INTERVAL " .
-		$row["topic_retention"] . " DAY)";
-	$rs_topic = mysqli_query($db_conn, $sql);
-	if ($rs_topic == false)
+	$sql = "DELETE FROM email WHERE complete AND set_dt < SUBDATE(NOW(), INTERVAL 180 DAY)";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
 	{
-		echo ("Query expired topic error: " . mysqli_error($db_conn));
+		echo ("Delete mail error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	$sql = "UPDATE email SET complete = 0, error = 0
+			WHERE error = 1 AND set_dt > SUBDATE(NOW(), INTERVAL 7 DAY)";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo ("Update email status error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	$sql = "DELETE FROM visit_log WHERE dt < SUBDATE(NOW(), INTERVAL $BBS_normal_log_retention DAY)";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo ("Delete visit_log error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	$sql = "DELETE FROM user_online WHERE last_tm < SUBDATE(NOW(), INTERVAL $BBS_session_lifetime SECOND)";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo ("Delete online user error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	$sql = "DELETE FROM user_login_log WHERE login_dt < SUBDATE(NOW(), INTERVAL $BBS_critical_log_retention DAY)";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo ("Delete user_login_log error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	$sql = "DELETE FROM user_err_login_log WHERE login_dt < SUBDATE(NOW(), INTERVAL $BBS_normal_log_retention DAY)";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo ("Delete user_err_login_log error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	$sql = "DELETE FROM bbs_article_op WHERE op_dt < SUBDATE(NOW(), INTERVAL $BBS_critical_log_retention DAY)";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo ("Delete bbs_article_op error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	$sql = "DELETE FROM bbs_msg WHERE send_dt < SUBDATE(NOW(), INTERVAL $BBS_user_msg_retention DAY) AND (NOT new)";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo ("Delete bbs_msg error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	$sql = "DELETE FROM view_article_log WHERE dt < SUBDATE(NOW(), INTERVAL $BBS_new_article_period DAY)";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo ("Delete view_article_log error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	//Cleanup out-of-date non-excerptional topic
+	$sql = "SELECT SID, topic_retention FROM section_config
+			WHERE enable AND topic_retention > 0";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo ("Query section info error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	while ($row = mysqli_fetch_array($rs))
+	{
+		$sql = "SELECT AID FROM bbs WHERE TID = 0 AND SID = " . $row["SID"] .
+				" AND visible = 1 AND excerption = 0 AND sub_dt < SUBDATE(NOW(), INTERVAL " .
+				$row["topic_retention"] . " DAY)";
+
+		$rs_topic = mysqli_query($db_conn, $sql);
+		if ($rs_topic == false)
+		{
+			echo ("Query expired topic error: " . mysqli_error($db_conn));
+			exit();
+		}
+
+		$aid_list = "-1";
+		while ($row_topic = mysqli_fetch_array($rs_topic))
+		{
+			$aid_list .= (", " . $row_topic["AID"]);
+		}
+		mysqli_free_result($rs_topic);
+
+		if ($aid_list != "-1")
+		{
+			// Reserve excerption reply as topic
+			$sql = "UPDATE bbs SET TID = 0 WHERE TID IN ($aid_list) AND excerption = 1";
+			$ret = mysqli_query($db_conn, $sql);
+			if ($ret == false)
+			{
+				echo ("Update reply error: " . mysqli_error($db_conn));
+				exit();
+			}
+
+			echo ("Convert " . mysqli_affected_rows($db_conn) . " replies to topics in section [" .
+					$row["SID"] . "]<br />\n");
+
+			$sql = "UPDATE bbs SET visible = 0, reply_count = 0 WHERE AID IN ($aid_list) OR TID IN ($aid_list)";
+			$ret = mysqli_query($db_conn, $sql);
+			if ($ret == false)
+			{
+				echo ("Update expired article error: " . mysqli_error($db_conn));
+				exit();
+			}
+
+			echo ("Deleted " . mysqli_affected_rows($db_conn) . " articles from section [" .
+				$row["SID"] . "]<br />\n");
+		}
+	}
+	mysqli_free_result($rs);
+
+	//Purge out-of-date deleted article
+	$sql = "SELECT bbs.AID FROM bbs LEFT JOIN bbs_article_op
+			ON bbs.AID = bbs_article_op.AID
+			WHERE visible = 0 AND bbs_article_op.AID IS NULL
+			AND sub_dt < SUBDATE(NOW(), INTERVAL $BBS_article_purge_duration DAY)";
+
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo ("Update deleted article error: " . mysqli_error($db_conn));
 		exit();
 	}
 
 	$aid_list = "-1";
-	while ($row_topic = mysqli_fetch_array($rs_topic))
+	while ($row = mysqli_fetch_array($rs))
 	{
-		$aid_list .= (", " . $row_topic["AID"]);
+		$aid_list .= (", " . $row["AID"]);
+		$aid_count++;
 	}
-	mysqli_free_result($rs_topic);
+	mysqli_free_result($rs);
 
 	if ($aid_list != "-1")
 	{
-		// Reserve excerption reply as topic
-		$sql = "UPDATE bbs SET TID = 0 WHERE TID IN ($aid_list) AND excerption = 1";
+		$sql = "DELETE FROM bbs_content WHERE AID IN ($aid_list)";
 		$rs = mysqli_query($db_conn, $sql);
 		if ($rs == false)
 		{
-			echo ("Update reply error: " . mysqli_error($db_conn));
+			echo ("Delete content error: " . mysqli_error($db_conn));
 			exit();
 		}
-
-		echo ("Convert " . mysqli_affected_rows($db_conn) . " replies to topics in section [" .
-				$row["SID"] . "]<br />\n");
-
-		$sql = "UPDATE bbs SET visible = 0, reply_count = 0 WHERE AID IN ($aid_list) OR TID IN ($aid_list)";
+		
+		$sql = "DELETE FROM bbs WHERE AID IN ($aid_list)";
 		$rs = mysqli_query($db_conn, $sql);
 		if ($rs == false)
 		{
-			echo ("Update expired article error: " . mysqli_error($db_conn));
+			echo ("Delete article error: " . mysqli_error($db_conn));
 			exit();
 		}
-
-		echo ("Deleted " . mysqli_affected_rows($db_conn) . " articles from section [" .
-			$row["SID"] . "]<br />\n");
+		
+		echo ("Purged " . mysqli_affected_rows($db_conn) . " articles<br />\n");
 	}
-}
-mysqli_free_result($rs);
 
-//Purge out-of-date deleted article
-$sql = "SELECT bbs.AID FROM bbs LEFT JOIN bbs_article_op
-		ON bbs.AID = bbs_article_op.AID
-		WHERE visible = 0 AND bbs_article_op.AID IS NULL
-		AND sub_dt < SUBDATE(NOW(), INTERVAL $BBS_article_purge_duration DAY)";
-
-$rs = mysqli_query($db_conn, $sql);
-if ($rs == false)
-{
-	echo ("Update deleted article error: " . mysqli_error($db_conn));
-	exit();
-}
-
-$aid_list = "-1";
-while ($row = mysqli_fetch_array($rs))
-{
-	$aid_list .= (", " . $row["AID"]);
-	$aid_count++;
-
-}
-mysqli_free_result($rs);
-
-if ($aid_list != "-1")
-{
-	$sql = "DELETE FROM bbs_content WHERE AID IN ($aid_list)";
+	//Delete expired upload file
+	$sql = "SELECT upload_file.AID FROM upload_file
+			INNER JOIN bbs ON upload_file.ref_AID = bbs.AID
+			WHERE upload_file.deleted = 0 AND upload_file.deny = 0 AND bbs.visible = 1";
 	$rs = mysqli_query($db_conn, $sql);
 	if ($rs == false)
 	{
-		echo ("Delete content error: " . mysqli_error($db_conn));
+		echo("Query upload_file error" . mysqli_error($db_conn));
 		exit();
 	}
-	
-	$sql = "DELETE FROM bbs WHERE AID IN ($aid_list)";
+
+	$upload_reserve_list="-1";
+	$file_reserved = array();
+	while ($row = mysqli_fetch_array($rs))
+	{
+		$upload_reserve_list .= (", " . $row["AID"]);
+		array_push($file_reserved, $row["AID"]);
+	}
+	mysqli_free_result($rs);
+
+	$sql = "UPDATE upload_file SET deleted = 1 WHERE AID NOT IN ($upload_reserve_list)";
 	$rs = mysqli_query($db_conn, $sql);
 	if ($rs == false)
 	{
-		echo ("Delete article error: " . mysqli_error($db_conn));
+		echo("Update upload_file error" . mysqli_error($db_conn));
 		exit();
 	}
-	
-	echo ("Purged " . mysqli_affected_rows($db_conn) . " articles<br />\n");
-}
 
-//Delete expired upload file
-$sql = "SELECT upload_file.AID FROM upload_file
-		INNER JOIN bbs ON upload_file.ref_AID = bbs.AID
-		WHERE upload_file.deleted = 0 AND upload_file.deny = 0 AND bbs.visible = 1";
-$rs = mysqli_query($db_conn, $sql);
-if ($rs == false)
-{
-	echo("Query upload_file error" . mysqli_error($db_conn));
-	exit();
-}
-
-$upload_reserve_list="-1";
-$file_reserved = array();
-while ($row = mysqli_fetch_array($rs))
-{
-	$upload_reserve_list .= (", " . $row["AID"]);
-	array_push($file_reserved, $row["AID"]);
-}
-mysqli_free_result($rs);
-
-$sql = "UPDATE upload_file SET deleted = 1 WHERE AID NOT IN ($upload_reserve_list)";
-$rs = mysqli_query($db_conn, $sql);
-if ($rs == false)
-{
-	echo("Update upload_file error" . mysqli_error($db_conn));
-	exit();
-}
-
-if (file_exists("../bbs/upload"))
-{
-	$handle = opendir("../bbs/upload"); 
-	while (false !== ($file = readdir($handle))) 
+	if (file_exists("../bbs/upload"))
 	{
-		if ($file != "." && $file != ".." && !in_array($file, $file_reserved))
+		$handle = opendir("../bbs/upload"); 
+		while (false !== ($file = readdir($handle))) 
 		{
-			unlink("../bbs/upload/$file");
-		} 
+			if ($file != "." && $file != ".." && !in_array($file, $file_reserved))
+			{
+				unlink("../bbs/upload/$file");
+			} 
+		}
+		closedir($handle); 
 	}
-	closedir($handle); 
-}
 
-//Delete expired upload photo
-$sql = "SELECT UID, photo_ext FROM user_pubinfo
-		WHERE photo = 999 AND photo_enable = 1";
-$rs = mysqli_query($db_conn, $sql);
-if ($rs == false)
-{
-	echo("Query user photo error" . mysqli_error($db_conn));
-	exit();
-}
-
-$file_reserved = array();
-while ($row = mysqli_fetch_array($rs))
-{
-	array_push($file_reserved, "face_" . $row["UID"] . "." . $row["photo_ext"]);
-}
-mysqli_free_result($rs);
-
-if (file_exists("../bbs/images/face/upload_photo"))
-{
-	$handle = opendir("../bbs/images/face/upload_photo"); 
-	while (false !== ($file = readdir($handle))) 
+	//Delete expired upload photo
+	$sql = "SELECT UID, photo_ext FROM user_pubinfo
+			WHERE photo = 999 AND photo_enable = 1";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
 	{
-		if ($file != "." && $file != ".." && !in_array($file, $file_reserved))
-		{
-			unlink("../bbs/images/face/upload_photo/$file");
-		} 
+		echo("Query user photo error" . mysqli_error($db_conn));
+		exit();
 	}
-	closedir($handle); 
-}
 
-//Purge dead ID
-$life_list = "-1";
-foreach ($BBS_life_immortal as $life)
-{
-	$life_list .= (", " . $life);
-}
-
-$sql = "SELECT UID FROM user_pubinfo WHERE life NOT IN ($life_list)
-		AND last_login_dt < SUBDATE(NOW(), INTERVAL (life + $BBS_user_purge_duration) DAY)
-		ORDER BY UID";
-$rs = mysqli_query($db_conn, $sql);
-if ($rs == false)
-{
-	echo("Query user_pubinfo error" . mysqli_error($db_conn));
-	exit();
-}
-
-$uid_list = "-1";
-while ($row = mysqli_fetch_array($rs))
-{
-	$uid_list .= (", " . $row["UID"]);
-}
-mysqli_free_result($rs);
-
-if ($uid_list != "-1")
-{
-	echo ("Purge UID in list ($uid_list)<br />\n");
-
-	$user_db = array(
-		"bbs_msg" => "toUID",
-		"section_favorite" => "UID",
-		"user_pubinfo" => "UID",
-		"user_list" => "UID",
-		"user_nickname" => "UID",
-		"user_modify_log" => "UID",
-		"user_reginfo" => "UID",
-		"user_score" => "UID",
-		"user_score_log" => "UID",
-		"user_modify_email_verify" => "UID",
-	);
-	
-	foreach($user_db as $table => $column)
+	$file_reserved = array();
+	while ($row = mysqli_fetch_array($rs))
 	{
-		$sql = "DELETE from $table WHERE $column IN ($uid_list)";
+		array_push($file_reserved, "face_" . $row["UID"] . "." . $row["photo_ext"]);
+	}
+	mysqli_free_result($rs);
 
-		$rs = mysqli_query($db_conn, $sql);
-		if ($rs == false)
+	if (file_exists("../bbs/images/face/upload_photo"))
+	{
+		$handle = opendir("../bbs/images/face/upload_photo"); 
+		while (false !== ($file = readdir($handle))) 
 		{
-			echo("Delete $table error: " . mysqli_error($db_conn));
-			exit();
+			if ($file != "." && $file != ".." && !in_array($file, $file_reserved))
+			{
+				unlink("../bbs/images/face/upload_photo/$file");
+			} 
+		}
+		closedir($handle); 
+	}
+
+	//Purge dead ID
+	$life_list = "-1";
+	foreach ($BBS_life_immortal as $life)
+	{
+		$life_list .= (", " . $life);
+	}
+
+	$sql = "SELECT UID FROM user_pubinfo WHERE life NOT IN ($life_list)
+			AND last_login_dt < SUBDATE(NOW(), INTERVAL (life + $BBS_user_purge_duration) DAY)
+			ORDER BY UID";
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo("Query user_pubinfo error" . mysqli_error($db_conn));
+		exit();
+	}
+
+	$uid_list = "-1";
+	while ($row = mysqli_fetch_array($rs))
+	{
+		$uid_list .= (", " . $row["UID"]);
+	}
+	mysqli_free_result($rs);
+
+	if ($uid_list != "-1")
+	{
+		echo ("Purge UID in list ($uid_list)<br />\n");
+
+		$user_db = array(
+			"bbs_msg" => "toUID",
+			"section_favorite" => "UID",
+			"user_pubinfo" => "UID",
+			"user_list" => "UID",
+			"user_nickname" => "UID",
+			"user_modify_log" => "UID",
+			"user_reginfo" => "UID",
+			"user_score" => "UID",
+			"user_score_log" => "UID",
+			"user_modify_email_verify" => "UID",
+		);
+		
+		foreach($user_db as $table => $column)
+		{
+			$sql = "DELETE from $table WHERE $column IN ($uid_list)";
+
+			$rs = mysqli_query($db_conn, $sql);
+			if ($rs == false)
+			{
+				echo("Delete $table error: " . mysqli_error($db_conn));
+				exit();
+			}
 		}
 	}
-}
 
-mysqli_close($db_conn);
+	mysqli_close($db_conn);
 ?>
-<P style="color:brown">数据库整理完毕！ </P>
+<P>数据库整理完毕！</P>
 </body>
 </html>
--- manage/db_repair.php
+++ manage/db_repair.php
@@ -8,15 +8,14 @@
 	require_once "../lib/lml.inc.php";
 	require_once "../lib/str_process.inc.php";
 	require_once "../bbs/session_init.inc.php";
-?>
-<?
-force_login();
 
-if (!$_SESSION["BBS_priv"]->checklevel(P_ADMIN_M) && !isset($_SERVER["argc"]))
-{
-	echo ("没有权限！");
-	exit();
-} 
+	force_login();
+
+	if (!$_SESSION["BBS_priv"]->checklevel(P_ADMIN_M) && !isset($_SERVER["argc"]))
+	{
+		echo ("没有权限！");
+		exit();
+	} 
 ?>
 <html>
 <head>
@@ -25,72 +24,99 @@ if (!$_SESSION["BBS_priv"]->checklevel(P_ADMIN_M) && !isset($_SERVER["argc"]))
 <link rel="stylesheet" href="css/default.css" type="text/css">
 </head>
 <body>
-<P style="color:brown">
+<P>
 <?
-$rs=mysql_query("select b1.AID,b1.reply_count as rc1,sum(b2.visible) as rc2".
-	" from bbs as b1 left join bbs as b2 on b1.AID=b2.TID where".
-	" b1.TID=0 and b1.visible".
-	" group by b1.AID having rc1<>rc2 order by b1.AID")
-	or die("Query reply error!");
+	$sql = "SELECT b1.AID, b1.reply_count AS rc1, SUM(b2.visible) AS rc2
+			FROM bbs AS b1 LEFT JOIN bbs AS b2 ON b1.AID = b2.TID
+			WHERE b1.TID = 0 AND b1.visible
+			GROUP BY b1.AID HAVING rc1 <> rc2
+			ORDER BY b1.AID";
 
-while($row=mysql_fetch_array($rs))
-{
-	mysql_query("update bbs set reply_count=".$row["rc2"].
-		" where AID=".$row["AID"])
-		or die("Update data error!");
-	echo ($row["AID"]."已修复回帖数"."<br>\n");
-}
-
-mysql_free_result($rs);
-
-$rs=mysql_query("select AID,SID from bbs where TID=0 and reply_count>0")
-	or die("Query data error!");
-
-while($row=mysql_fetch_array($rs))
-{
-	mysql_query("update bbs set SID=".$row["SID"]." where TID="
-		.$row["AID"])
-		or die("Update data error!");
-	if (($ar=mysql_affected_rows())>0)
-		echo ("已修复[".$row["AID"]."]主题的".$ar."回帖所属版块<br>\n");
-}
-
-mysql_free_result($rs);
-
-$sql = "SELECT bbs.AID, length, content FROM bbs
-		INNER JOIN bbs_content ON bbs.CID = bbs_content.CID
-		ORDER BY bbs.AID";
-
-$rs = mysqli_query($db_conn, $sql);
-if ($rs == false)
-{
-	echo("Query user_pubinfo error" . mysqli_error($db_conn));
-	exit();
-}
-
-while ($row = mysqli_fetch_array($rs))
-{
-	$content_f = LML($row["content"], false, false, 1024);
-	$length = str_length($content_f);
-	
-	if ($row["length"] != $length)
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
 	{
-		$sql = "UPDATE bbs SET length = $length WHERE AID = " . $row["AID"];
-		$rs_update = mysqli_query($db_conn, $sql);
-		if ($rs_update == false)
+		echo ("Query reply error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	while ($row = mysqli_fetch_array($rs))
+	{
+		$sql = "UPDATE bbs SET reply_count = " . $row["rc2"] .
+				" WHERE AID = " . $row["AID"];
+
+		$ret = mysqli_query($db_conn, $sql);
+		if ($ret == false)
 		{
-			echo("Update article error" . mysqli_error($db_conn));
+			echo ("Update data error: " . mysqli_error($db_conn));
 			exit();
 		}
-		
-		echo ("[" . $row["AID"] . "] " . $row["length"] . " => $length<br />\n");
-	}
-}
-mysqli_free_result($rs);
 
-mysql_close($db_conn);
+		echo ($row["AID"] . "已修复回帖数<br />\n");
+	}
+	mysqli_free_result($rs);
+
+	$sql = "SELECT AID, SID FROM bbs WHERE TID = 0 AND reply_count > 0";
+
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo ("Query data error: " . mysqli_error($db_conn));
+		exit();
+	}
+
+	while ($row = mysqli_fetch_array($rs))
+	{
+		$sql = "UPDATE bbs SET SID = " . $row["SID"] . " WHERE TID = " . $row["AID"];
+
+		$ret = mysqli_query($db_conn, $sql);
+		if ($ret == false)
+		{
+			echo ("Update data error: " . mysqli_error($db_conn));
+			exit();
+		}
+
+		$ar = mysqli_affected_rows($db_conn);
+		if ($ar > 0)
+		{
+			echo ("已修复[" . $row["AID"] . "]主题的" . $ar . "回帖所属版块<br />\n");
+		}
+	}
+	mysqli_free_result($rs);
+
+	$sql = "SELECT bbs.AID, length, content FROM bbs
+			INNER JOIN bbs_content ON bbs.CID = bbs_content.CID
+			ORDER BY bbs.AID";
+
+	$rs = mysqli_query($db_conn, $sql);
+	if ($rs == false)
+	{
+		echo("Query user_pubinfo error" . mysqli_error($db_conn));
+		exit();
+	}
+
+	while ($row = mysqli_fetch_array($rs))
+	{
+		$content_f = LML($row["content"], false, false, 1024);
+		$length = str_length($content_f);
+		
+		if ($row["length"] != $length)
+		{
+			$sql = "UPDATE bbs SET length = $length WHERE AID = " . $row["AID"];
+			$ret = mysqli_query($db_conn, $sql);
+			if ($ret == false)
+			{
+				echo("Update article error" . mysqli_error($db_conn));
+				exit();
+			}
+			
+			echo ("[" . $row["AID"] . "] " . $row["length"] . " => $length<br />\n");
+		}
+	}
+	mysqli_free_result($rs);
+
+	mysqli_close($db_conn);
 ?>
 </p>
-<P style="color:brown">数据库修复完毕！ </P>
+<P>数据修复完毕！</P>
 </body>
 </html>
