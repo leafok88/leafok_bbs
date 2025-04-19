a949e797efc0743335da45f65495b28b89ad28b4
Leaflet <leaflet@leafok.com>
2025-04-19 13:00:28 +0800

    Add setting for DB-side TZ

--- TODO/conf/db_conn.conf.php
+++ TODO/conf/db_conn.conf.php
@@ -1,6 +1,8 @@
 <?
-	$DB_hostname = "127.0.0.1";
-	$DB_username = "leafok";
-	$DB_password = "123456";
-	$DB_database = "leafok";
+	$DB_hostname			=	"127.0.0.1";
+	$DB_username			=	"leafok";
+	$DB_password			=	"123456";
+	$DB_database			=	"leafok";
+
+	$DB_session_timezone	=	"+08:00"; //Keep consistent with DB server config
 ?>
