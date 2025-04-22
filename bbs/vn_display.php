db34c948d215b0449370404ba6e0d5ea72e98a71
Leaflet <leaflet@leafok.com>
2025-04-22 20:44:23 +0800

    Refine VN

--- bbs/vn_display.php
+++ bbs/vn_display.php
@@ -1,7 +1,7 @@
 <?
-	require_once "./session_init.inc.php";
 	require_once "../lib/vn_gif.inc.php";
-?>
-<?
+	require_once "./session_init.inc.php";
+
 	$_SESSION["BBS_vn_str"] = VN_gen_str(4);
+	VN_gif_display($_SESSION["BBS_vn_str"]);
 ?>
