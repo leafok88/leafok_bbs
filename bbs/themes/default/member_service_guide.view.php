c7603ec6f33d68b8cbed1ee8b4219f8db159e61c
Leaflet <leaflet@leafok.com>
2025-04-25 15:20:28 +0800

    Refact

--- bbs/themes/default/member_service_guide.view.php
+++ bbs/themes/default/member_service_guide.view.php
@@ -1,3 +1,15 @@
+<?php
+	if ($_SESSION["BBS_priv"]->checkpriv(0, S_POST) &&
+		!$_SESSION["BBS_priv"]->checklevel(P_ADMIN_M | P_ADMIN_S | P_MAN_M | P_MAN_S))
+	{
+		$close_account_url = "user_suicide.php";
+	}
+	else
+	{
+		$close_account_url = "#";
+	}
+
+	echo <<<HTML
 	<center>
 		<table cols="6" border="1" cellpadding="3" cellspacing="0" width="1050" bgcolor="#ffdead" id="Guide">
 			<tr>
@@ -14,22 +26,10 @@
 					<a class="s7" href="user_score_detail.php">账户积分</a>
 				</td>
 				<td width="16%" align="middle">
-<?php
-	if ($_SESSION["BBS_priv"]->checkpriv(0, S_POST) &&
-		!$_SESSION["BBS_priv"]->checklevel(P_ADMIN_M | P_ADMIN_S | P_MAN_M | P_MAN_S))
-	{
-?>
-					<a class="s7" href="user_suicide.php">关闭账户</a>
-<?php
-	}
-	else
-	{
-?>
-					<a class="s7" href="#">关闭账户</a>
-<?php
-	}
-?>
+					<a class="s7" href="{$close_account_url}">关闭账户</a>
 				</td>
 			</tr>
 		</table>
 	</center>
+	HTML;
+?>
