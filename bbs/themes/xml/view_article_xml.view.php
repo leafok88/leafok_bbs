6b6ceaf52ef0547af5bb5e7e01ebd098b2e757fb
Leaflet <leaflet@leafok.com>
2025-04-26 19:13:30 +0800

    Refact

--- bbs/themes/xml/view_article_xml.view.php
+++ bbs/themes/xml/view_article_xml.view.php
@@ -9,40 +9,64 @@
 	require_once "../lib/str_process.inc.php";
 	require_once "./user_level.inc.php";
 
+	$xsl_file = get_theme_file('xsl/1');
+
+	$title = htmlspecialchars($result_set["data"]["title"], ENT_HTML401, 'UTF-8');
+
 	header('Content-Type: text/xml; charset=UTF-8');
-	echo("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n");
-	echo("<?xml-stylesheet type='text/xsl' href='" . get_theme_file('xsl/1') . "' version='1.0'?>\n");
-?>
-<Topic>
-	<Subject>
-		<SectionId><?= $result_set["data"]["sid"]; ?></SectionId>
-		<SectionTitle><?= $result_set["data"]["section_title"]; ?></SectionTitle>
-		<TopicId><?= $result_set["data"]["id"]; ?></TopicId>
-		<TopicTitle><?= htmlspecialchars($result_set["data"]["title"], ENT_HTML401, 'UTF-8'); ?></TopicTitle>
-	</Subject>
-	<Articles>
-<?php
+
+	echo <<<HTML
+	<?xml version="1.0" encoding="UTF-8" ?>
+	<?xml-stylesheet type='text/xsl' href='{$xsl_file}' version='1.0'?>
+	<Topic>
+		<Subject>
+			<SectionId>{$result_set["data"]["sid"]}</SectionId>
+			<SectionTitle>{$result_set["data"]["section_title"]}</SectionTitle>
+			<TopicId>{$result_set["data"]["id"]}</TopicId>
+			<TopicTitle>{$title}</TopicTitle>
+		</Subject>
+		<Articles>
+	HTML;
+
 	foreach ($result_set["data"]["articles"] as $article)
 	{
-?>
-		<Article>
-			<PostUserId><?= $article["uid"]; ?></PostUserId>
-			<PostUserName><?= htmlspecialchars($article["username"], ENT_HTML401, 'UTF-8'); ?></PostUserName>
-			<PostUserNickName><?= htmlspecialchars($article["nickname"], ENT_HTML401, 'UTF-8'); ?></PostUserNickName>
-			<rank><?= user_level($article["exp"]); ?></rank>
-			<credit><?= $article["exp"]; ?></credit>
-			<photo><?= $article["photo_path"]; ?></photo>
-			<ArticleId><?= $article["aid"]; ?></ArticleId>
-			<ArticleTitle><?= htmlspecialchars($article["title"], ENT_HTML401, 'UTF-8'); ?><?php if ($article["transship"]) { ?> [转载]<?php } ?></ArticleTitle>
-			<ExpressionIcon><?= $article["icon"]; ?></ExpressionIcon>
-			<PostDateTime><?= $article["sub_dt"]->format("Y-m-d H:i:s (\U\T\C P)"); ?></PostDateTime>
-			<PostIP><?= $article["sub_ip"]; ?></PostIP>
-			<Content><![CDATA[<?= LML($article["content"], true, false, 1024); ?>]]></Content>
-			<Length><?= $article["length"]; ?></Length>
-			<Visible><?= $article["visible"]; ?></Visible>
-		</Article>
-<?php
+		$username = htmlspecialchars($article["username"], ENT_HTML401, 'UTF-8');
+		$nickname = htmlspecialchars($article["nickname"], ENT_HTML401, 'UTF-8');
+		$level = user_level($article["exp"]);
+		$content = LML($article["content"], true, false, 1024);
+
+		$transship_info = "";
+		if ($article["transship"])
+		{
+			$transship_info = <<<HTML
+				 [转载]
+			HTML;
+		}
+
+		echo <<< HTML
+			<Article>
+				<PostUserId>{$article["uid"]}</PostUserId>
+				<PostUserName>{$username}</PostUserName>
+				<PostUserNickName>{$nickname}</PostUserNickName>
+				<rank>{$level}</rank>
+				<credit>{$article["exp"]}</credit>
+				<photo>{$article["photo_path"]}</photo>
+				<ArticleId>{$article["aid"]}</ArticleId>
+				<ArticleTitle>{$title}{$transship_info}</ArticleTitle>
+				<ExpressionIcon>{$article["icon"]}</ExpressionIcon>
+				<PostDateTime>{$article["sub_dt"]->format("Y-m-d H:i:s (\U\T\C P)")}</PostDateTime>
+				<PostIP>{$article["sub_ip"]}</PostIP>
+				<Content><![CDATA[{$content}]]></Content>
+				<Length>{$article["length"]}</Length>
+				<Visible>{$article["visible"]}</Visible>
+			</Article>
+			
+		HTML;
 	}
-?>		
-	</Articles>
-</Topic>
+
+	echo <<<HTML
+		</Articles>
+	</Topic>
+	HTML;
+?>
+
