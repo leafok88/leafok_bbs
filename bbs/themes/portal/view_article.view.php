901021f39ef77e8516d543e6a3790e33f8a05234
Leaflet <leaflet@leafok.com>
2025-04-27 11:57:21 +0800

    Refine

--- bbs/themes/portal/view_article.view.php
+++ bbs/themes/portal/view_article.view.php
@@ -19,6 +19,7 @@
 	<html>
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<base href="/www/" />
 	<title>{$BBS_name} - {$title}</title>
 	<link rel="stylesheet" href="{$css_file}" type="text/css">
 	<style type="text/css">
