6675d6b82bb27041279402b49a09abb2e264cbcf
Leaflet <leaflet@leafok.com>
2025-04-27 11:46:15 +0800

    Refact

--- bbs/themes/gen_ex/gen_ex_article.view.php
+++ bbs/themes/gen_ex/gen_ex_article.view.php
@@ -9,21 +9,43 @@
 	require_once "../lib/lml.inc.php";
 	require_once "../lib/str_process.inc.php";
 
-	$section_path = ($result_set["data"]["ex_dir"] != null ? str_repeat("../", substr_count($result_set["data"]["ex_dir"], "/")) : "");
-
 	// Pre-defined color setting of article display
 	$color = array(
 		"#FAFBFC",
 		"#f0F3Fa"
 	);
 	$color_index = 0;
-?>
-<html>
+
+	$section_path = ($result_set["data"]["ex_dir"] != null ? str_repeat("../", substr_count($result_set["data"]["ex_dir"], "/")) : "");
+
+	$title = htmlspecialchars($result_set["data"]["title"], ENT_HTML401, 'UTF-8');
+
+	$ex_dir_link = "";
+	if ($result_set["data"]["ex_dir"] != null)
+	{
+		$ex_dir_link = <<<HTML
+		&gt;&gt;<a href="index.html">{$result_set["data"]["ex_name"]}</a>
+		HTML;
+	}
+
+	echo <<<HTML
+	<html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<title><?= htmlspecialchars($result_set["data"]["title"], ENT_HTML401, 'UTF-8'); ?></title>
-		<link rel="stylesheet" href="<?= $section_path; ?>../article.css" type="text/css">
-		<script type="text/javascript" src="<?= $section_path; ?>../img_adjust.js"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<base href="/gen_ex/{$result_set['data']['sid']}/{$result_set['data']['ex_dir']}{$result_set['data']['id']}.html" />
+	<title>{$title}</title>
+	<link rel="stylesheet" href="{$section_path}../article.css" type="text/css">
+	<style type="text/css">
+	SPAN.title_normal
+	{
+		color: #909090;
+	}
+	TD.content_normal
+	{
+		font-size: 16px;
+	}
+	</style>
+	<script type="text/javascript" src="{$section_path}../img_adjust.js"></script>
 	</head>
 	<body>
 		<a name="top"></a>
@@ -31,120 +53,67 @@
 			<table border="0" cellpadding="0" cellspacing="0" width="1050">
 				<tr>
 					<td>
-						<a href="<?= $section_path; ?>../index.html"><?= $BBS_name; ?>精华区</a>&gt;&gt;<a href="<?= $section_path; ?>index.html"><?= $result_set["data"]["section_title"]; ?></a><?php
-						if ($result_set["data"]["ex_dir"] != null) {
-						?>&gt;&gt;<a href="index.html"><?= $result_set["data"]["ex_name"]; ?></a><?php
-						}
-						?>
+						<a href="{$section_path}../index.html"><?= $BBS_name; ?>精华区</a>&gt;&gt;<a href="{$section_path}index.html">{$result_set["data"]["section_title"]}</a>{$ex_dir_link}
 					</td>
 				</tr>
 				<tr bgcolor="#d0d3F0" height="25">
 					<td align="center" class="title">
-						[<?= $result_set["data"]["id"]; ?>]&nbsp;主题：&nbsp;<?= htmlspecialchars($result_set["data"]["title"], ENT_HTML401, 'UTF-8'); ?>
+						[{$result_set["data"]["id"]}]&nbsp;主题：&nbsp;{$title}
 					</td>
 				</tr>
 			</table>
-<?php
+	HTML;
+
 	foreach ($result_set["data"]["articles"] as $article)
 	{
 		$color_index = ($color_index + 1) % count($color);
 
+		$username = htmlspecialchars($article["username"], ENT_HTML401, 'UTF-8');
+		$nickname = htmlspecialchars($article["nickname"], ENT_HTML401, 'UTF-8');
+		$title_f = split_line(htmlspecialchars($article["title"], ENT_HTML401, 'UTF-8'), "", 65, 2, "<br />");
+		$content_f = LML(htmlspecialchars($article["content"], ENT_HTML401, 'UTF-8'), true, true, 80);
+
+		$transship_info = "";
+		if ($article["transship"])
+		{
+			$transship_info = <<<HTML
+				<font color="red">[转载]</font>
+			HTML;
+		}
+
 		if ($article["tid"] != 0)
 		{
-?>
-			<a name="<?= $article["aid"]; ?>"></a>
-			<table bgcolor="<?= $color[$color_index]; ?>" border="0" cellpadding="0" cellspacing="0" width="1050">
-				<tr height="1" bgcolor="#202020">
-					<td colspan="3">
-					</td>
-				</tr>
-			</table>
-<?php
+			echo <<<HTML
+				<a name="{$article['aid']}"></a>
+				<table border="0" cellpadding="0" cellspacing="0" width="1050">
+					<tr height="1" bgcolor="#202020">
+						<td>
+						</td>
+					</tr>
+				</table>
+			HTML;
 		}
-?>
-			<table bgcolor="<?= $color[$color_index]; ?>" border="0" cellpadding="0" cellspacing="10" width="1050">
-				<tr>
-					<td width="5%">
-					</td>
-					<td width="90%" class="body">
-						<span style="color:#606060; ">作者：</span>&nbsp;<span style="color: #909090; "><?= htmlspecialchars($article["username"], ENT_HTML401, 'UTF-8'); ?> (<?= htmlspecialchars($article["nickname"], ENT_HTML401, 'UTF-8'); ?>)</span>
-					</td>
-					<td width="5%">
-					</td>
-				</tr>
-				<tr>
-					<td>
-					</td>
-					<td class="body">
-						<span style="color:#606060; ">标题：</span>&nbsp;<span style="color: #909090; "><?= split_line(htmlspecialchars($article["title"], ENT_HTML401, 'UTF-8'), "", 65, 2, "<br />"); ?></span><?php if ($article["transship"]) { ?><font color="red">[转载]</font><?php } ?>
-					</td>
-					<td>
-					</td>
-				</tr>
-				<tr>
-					<td>
-					</td>
-					<td class="body">
-						<span style="color:#606060; ">来自：</span>&nbsp;<span style="color: #909090; "><?= $article["sub_ip"]; ?></span>
-					</td>
-					<td>
-					</td>
-				</tr>
-				<tr>
-					<td>
-					</td>
-					<td class="body">
-						<span style="color:#606060; ">发贴时间：</span>&nbsp;<span style="color: #909090; "><?= $article["sub_dt"]->format("Y年m月d日 H:i:s (\U\T\C P)"); ?></span>
-					</td>
-					<td>
-					</td>
-				</tr>
-				<tr>
-					<td>
-					</td>
-					<td class="body">
-						<span style="color:#606060; ">长度：</span>&nbsp;<span style="color: #909090; "><?= $article["length"]; ?>字</span>
-					</td>
-					<td>
-					</td>
-				</tr>
-				<tr height="2">
-					<td>
-					</td>
-					<td style="background-color: #909090; ">
-					</td>
-					<td>
-					</td>
-				</tr>
-				<tr>
-					<td>
-					</td>
-					<td style="font-size: 16px; ">
-						<pre><?= LML(htmlspecialchars($article["content"], ENT_HTML401, 'UTF-8'), true, true, 80); ?></pre>
-					</td>
-					<td>
-					</td>
-				</tr>
-				<tr>
-					<td>
-					</td>
-					<td style="color:#000000; ">
-						========== * * * * * ==========
-						<br>
-<?php
+
+		$atta_list = "";
 		foreach ($article["attachments"] as $attachment)
 		{
+			if (!$attachment["check"])
+			{
+				continue;
+			}
+
 			$filename = $attachment["filename"];
 			$ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 
 			if (!copy("../bbs/upload/" . $attachment["aid"], "../gen_ex/attachment/" . $attachment["aid"] . ".$ext"))
 			{
-				echo ("Copy file error!");
-				exit();
+				continue;
 			}
-?>
-						<a class="s2" href="<?= $section_path; ?>../attachment/<?= $attachment["aid"] . ".$ext"; ?>" target="_target"><?= $filename; ?></a> (<?= $attachment["size"]; ?>字节)<br>
-<?php
+
+			$atta_list .= <<<HTML
+				<a class="s2" href="{$section_path}../attachment/{$attachment['aid']}.$ext" target="_blank">{$filename}</a> ({$attachment["size"]}字节)
+			HTML;
+
 			switch ($ext)
 			{
 				case "bmp":
@@ -154,22 +123,103 @@
 				case "png":
 				case "tif":
 				case "tiff":
-?>
-						<br /><img onmousewheel="return bbs_img_zoom(event, this)" src="<?= $section_path; ?>../attachment/<?= $attachment["aid"] . ".$ext"; ?>">
-<?php
+					$atta_list .= <<<HTML
+						<br />
+						<img onmousewheel="return bbs_img_zoom(event, this)" src="{$section_path}../attachment/{$attachment['aid']}.$ext">
+					HTML;
 					break;
 			}
-?><br /><?php
+
+			$atta_list .= <<<HTML
+				<br />
+			HTML;
 		}
-?>
+
+		echo <<<HTML
+		<table bgcolor="{$color[$color_index]}" border="0" cellpadding="0" cellspacing="10" width="1050">
+			<tr>
+				<td width="5%">
+				</td>
+				<td width="90%" class="body">
+					<span style="color: #606060">作者：</span>&nbsp;<span style="color: #909090">{$username} ({$nickname})</span>
+				</td>
+				<td width="5%">
+				</td>
+			</tr>
+			<tr>
+				<td>
+				</td>
+				<td class="body">
+					<span style="color: #606060">标题：</span>
+					<img src="{$section_path}../../bbs/images/expression/{$article['icon']}.gif">
+					<span class="title_normal">
+						{$title_f}
+					</span>
+					{$transship_info}
+				</td>
+				<td>
+				</td>
+			</tr>
+			<tr>
+				<td>
+				</td>
+				<td class="body">
+					<span style="color: #606060">来自：</span>&nbsp;<span style="color: #909090">{$article["sub_ip"]}</span>
+				</td>
+				<td>
+				</td>
+			</tr>
+			<tr>
+				<td>
+				</td>
+				<td class="body">
+					<span style="color: #606060">发贴时间：</span>&nbsp;<span style="color: #909090">{$article["sub_dt"]->format("Y年m月d日 H:i:s (\U\T\C P)")}</span>
+				</td>
+				<td>
+				</td>
+			</tr>
+			<tr>
+				<td>
+				</td>
+				<td class="body">
+					<span style="color: #606060">长度：</span>&nbsp;<span style="color: #909090">{$article["length"]}字</span>
+				</td>
+				<td>
+				</td>
+			</tr>
+			<tr height="2">
+				<td>
+				</td>
+				<td style="background-color: #909090">
+				</td>
+				<td>
+				</td>
+			</tr>
+			<tr>
+				<td>
+				</td>
+				<td class="content_normal">
+					<pre>{$content_f}</pre>
+				</td>
+				<td>
+				</td>
+			</tr>
+			<tr>
+				<td>
+				</td>
+				<td style="color: #000000">
+					========== * * * * * ==========
+					<br>
+					{$atta_list}
 					</td>
-					<td>
-					</td>
-				</tr>
-			</table>
-<?php
-	} 
-?>
+				<td>
+				</td>
+			</tr>
+		</table>
+		HTML;
+	}
+
+	echo <<<HTML
 			<table border="0" cellpadding="5" cellspacing="0" width="1050">
 				<tr bgcolor="#d0d3F0" height="10">
 					<td>
@@ -186,11 +236,13 @@
 				</tr>
 				<tr>
 					<td align="center">
-						Copyright &copy; <?= $BBS_copyright_duration; ?> <?= $BBS_name . "(" . $BBS_host_name . ")"; ?><br /> 
+						Copyright &copy; {$BBS_copyright_duration} {$BBS_name}({$BBS_host_name})<br />
 						All Rights Reserved
 					</td>
 				</tr>
 			</table>
 		</center>
 	</body>
-</html>
+	</html>
+	HTML;
+?>
