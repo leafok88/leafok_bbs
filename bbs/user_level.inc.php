9790a84699b37c025a30744bb34c197d2b1760da
Leaflet <leaflet@leafok.com>
2025-10-24 08:45:22 +0800

    Refine and add test case

--- bbs/user_level.inc.php
+++ bbs/user_level.inc.php
@@ -12,20 +12,38 @@ function user_level($e)
 	while ($l < $r)
 	{
 		$m = intdiv(($l + $r), 2);
-		if ($e < $BBS_exp[$m + 1])
+		if ($e < $BBS_exp[$m])
 		{
-			$r = $m;
+			$r = $m - 1;
 		}
-		else if ($e > $BBS_exp[$m + 1])
+		else if ($e > $BBS_exp[$m])
 		{
 			$l = $m + 1;
 		}
-		else // if ($e == $BBS_exp[$m + 1])
+		else // if ($e == $BBS_exp[$m])
 		{
-			$l = $m + 1;
+			$l = $m;
 			break;
 		}
 	}
 
+	if ($e < $BBS_exp[$l])
+	{
+		$l--;
+	}
+
 	return $BBS_level[$l];
 }
+
+function test_user_level()
+{
+	$user_points = array(
+		-50, -1, 0, 1, 49, 50, 51, 499, 500, 501, 99999, 100000, 100001);
+
+	for ($i = 0; $i < count($user_points); $i++)
+	{
+		printf("%10d\t\t%s\n", $user_points[$i], user_level($user_points[$i]));
+	}
+}
+
+// test_user_level();
